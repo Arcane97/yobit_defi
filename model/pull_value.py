faef2cc8b36fb77723ac9201f0a93e8861e1ac66
Arcane97 <haker_ilnur_97@mail.ru>
2021-08-21 00:38:16 +0500

    Добавил получение данных о количестве валюты в пуле

---
+++ model/pull_value.py
@@ -0,0 +1,46 @@
+from bs4 import BeautifulSoup
+import requests
+
+from utils.constants import DOGE_BTC_DEFI_URL
+
+
+def get_yobit_page(url):
+    """ Получение страницы yobit
+    :param url: ссылка
+    :return: код страницы
+    """
+    try:
+        req = requests.get(url)
+    except Exception as e:
+        # todo доавить обработку ошибки
+        print(e)
+        return None
+
+    return req.text
+
+
+def get_pull_value(page_text):
+    """ Получение данных о количестве валюты в пуле
+    :param page_text: текст кода страницы
+    :return: данные о пуле
+    """
+    try:
+        soup = BeautifulSoup(page_text, 'lxml')
+        div_quotes = soup.find_all('div', class_='top_meta_mining')[1]
+        span_quotes = div_quotes.find_all('span')
+
+        value1 = float(span_quotes[0].text)
+        value2 = float(span_quotes[1].text)
+
+    except Exception as e:
+        # todo обработать ошибку
+        print(e)
+        return None
+
+    return value1, value2
+
+
+if __name__ == "__main__":
+    text = get_yobit_page(DOGE_BTC_DEFI_URL)
+    value = get_pull_value(text)
+    print(value)
