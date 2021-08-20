b5d1f263f29d2479dd4909d3dbe1a8b26a1607ab
Arcane97 <haker_ilnur_97@mail.ru>
2021-08-21 02:56:09 +0500

    Добавитл словарь с валютными парами и ссылками на них

--- utils/constants.py
+++ utils/constants.py
@@ -1,7 +1,16 @@
-
 DOGE_BTC_DEFI_URL = "https://yobit.net/ru/defi/DOGE/BTC"
 ETH_BTC_DEFI_URL = "https://yobit.net/ru/defi/ETH/BTC"
 TRX_BTC_DEFI_URL = "https://yobit.net/ru/defi/TRX/BTC"
 XRP_BTC_DEFI_URL = "https://yobit.net/ru/defi/XRP/BTC"
 ETH_USDT_DEFI_URL = "https://yobit.net/ru/defi/ETH/USDT"
 BTC_USDT_DEFI_URL = "https://yobit.net/ru/defi/BTC/USDT"
+BTC_RUR_DEFI_URL = "https://yobit.net/ru/defi/BTC/RUR"
+
+pairs_urls_dict = {"DOGEBTC": DOGE_BTC_DEFI_URL,
+                   "ETHBTC": ETH_BTC_DEFI_URL,
+                   "TRXBTC": TRX_BTC_DEFI_URL,
+                   "XRPBTC": XRP_BTC_DEFI_URL,
+                   "ETHUSDT": ETH_USDT_DEFI_URL,
+                   "BTCUSDT": BTC_USDT_DEFI_URL,
+                   "BTCRUR": BTC_RUR_DEFI_URL,
+                   }
