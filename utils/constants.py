40789681568bd2b08ac8b00c61df6f6c0d589786
Arcane97 <haker_ilnur_97@mail.ru>
2021-08-21 00:41:54 +0500

    Добавил в константы ещё ссылок

--- utils/constants.py
+++ utils/constants.py
@@ -1,2 +1,7 @@
 
 DOGE_BTC_DEFI_URL = "https://yobit.net/ru/defi/DOGE/BTC"
+ETH_BTC_DEFI_URL = "https://yobit.net/ru/defi/ETH/BTC"
+TRX_BTC_DEFI_URL = "https://yobit.net/ru/defi/TRX/BTC"
+XRP_BTC_DEFI_URL = "https://yobit.net/ru/defi/XRP/BTC"
+ETH_USDT_DEFI_URL = "https://yobit.net/ru/defi/ETH/USDT"
+BTC_USDT_DEFI_URL = "https://yobit.net/ru/defi/BTC/USDT"
