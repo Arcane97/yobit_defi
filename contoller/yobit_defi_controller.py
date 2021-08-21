9ef8b83e3a1ac501fb4d9c7a4e8bfcdc1a3846f6
Arcane97 <haker_ilnur_97@mail.ru>
2021-08-21 22:55:19 +0500

    Добавил загрузку данных в модель

--- contoller/yobit_defi_controller.py
+++ contoller/yobit_defi_controller.py
@@ -12,3 +12,21 @@ class YobitDefiController:
         self._view = YobitDefiView(self, model, log_name)
 
         self._view.show()
+
+    def set_pair(self):
+        try:
+            pair = self._view.ui.pair_cmbox.currentText()
+            self._model.pair = pair
+        except Exception:
+            self._logger.exception('Ошибка при попытке считать комбобокс с парами')
+        else:
+            self._logger.info(f'В программу введена пара: {pair}')
+
+    def set_arbitrage(self):
+        try:
+            arbitrage = float(self._view.ui.arbitrage_ledit.text())
+            self._model.arbitrage = arbitrage
+        except:
+            self._logger.error('Введите арбитраж числом')
+        else:
+            self._logger.info(f'В программу введен арбитраж: {arbitrage}')
--- view/yobit_defi_view.py
+++ view/yobit_defi_view.py
@@ -3,6 +3,7 @@ import logging
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 from view.yobit_defi_view_ui import Ui_MainWindow
+from utils.constants import pairs_urls_dict
 
 
 class YobitDefiView(QMainWindow):
@@ -15,6 +16,25 @@ class YobitDefiView(QMainWindow):
 
         self._controller = controller
         self._model = model
+
+        self._load_params()
+        self._connect_signals()
+
+    def _load_params(self):
+        for pair in pairs_urls_dict.keys():
+            self.ui.pair_cmbox.addItem(pair)
+
+        arbitrage = str(2)
+        self.ui.arbitrage_ledit.setText(arbitrage)
+
+    def _connect_signals(self):
+        self.ui.working_btn.clicked.connect(self._working_btn_clicked)
+        self.ui.pair_cmbox.currentTextChanged.connect(self._controller.set_pair)
+        self.ui.arbitrage_ledit.editingFinished.connect(self._controller.set_arbitrage)
+
+    def _working_btn_clicked(self):
+        print(self._model.pair)
+        print(self._model.arbitrage)
 
 
 if __name__ == "__main__":
