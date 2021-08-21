069a4cc5021f18bb08de55aee79864ba1ab28790
Arcane97 <haker_ilnur_97@mail.ru>
2021-08-21 05:15:59 +0500

    Изменил словарь с ссылками

--- utils/constants.py
+++ utils/constants.py
@@ -2,15 +2,19 @@ DOGE_BTC_DEFI_URL = "https://yobit.net/ru/defi/DOGE/BTC"
 ETH_BTC_DEFI_URL = "https://yobit.net/ru/defi/ETH/BTC"
 TRX_BTC_DEFI_URL = "https://yobit.net/ru/defi/TRX/BTC"
 XRP_BTC_DEFI_URL = "https://yobit.net/ru/defi/XRP/BTC"
+WBTC_BTC_DEFI_URL = "https://yobit.net/ru/defi/WBTC/BTC"
 ETH_USDT_DEFI_URL = "https://yobit.net/ru/defi/ETH/USDT"
 BTC_USDT_DEFI_URL = "https://yobit.net/ru/defi/BTC/USDT"
-BTC_RUR_DEFI_URL = "https://yobit.net/ru/defi/BTC/RUR"
+ETH_USDC_DEFI_URL = "https://yobit.net/ru/defi/ETH/USDC"
+DOGE_USDT_DEFI_URL = "https://yobit.net/ru/defi/DOGE/USDT"
 
 pairs_urls_dict = {"DOGEBTC": DOGE_BTC_DEFI_URL,
                    "ETHBTC": ETH_BTC_DEFI_URL,
                    "TRXBTC": TRX_BTC_DEFI_URL,
                    "XRPBTC": XRP_BTC_DEFI_URL,
+                   "WBTCBTC": WBTC_BTC_DEFI_URL,
                    "ETHUSDT": ETH_USDT_DEFI_URL,
                    "BTCUSDT": BTC_USDT_DEFI_URL,
-                   "BTCRUR": BTC_RUR_DEFI_URL,
+                   "ETHUSDC": ETH_USDC_DEFI_URL,
+                   "DOGEUSDT": DOGE_USDT_DEFI_URL,
                    }
